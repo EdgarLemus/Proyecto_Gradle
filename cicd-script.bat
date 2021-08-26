e323dd550ccc85a40747501fa91730c43a9fccfa
EdgarLemus <31544878+EdgarLemus@users.noreply.github.com>
2021-08-26 13:23:47 -0500

    Update cicd-script.bat

--- cicd-script.bat
+++ cicd-script.bat
@@ -53,7 +53,7 @@ set "$appendDateTimeInFolderName=true"
 set "$assigneeUser=557058:5d9c8f2950c9170dc568fa92"
 
 :: Fully-qualitified name of the test result file
-set "$resultPath=@\"C:/etc\rc.d\init.d\jenkins\workspace\PipelineJira\target\cucumber\cucumber.json/"
+set "$resultPath=@/"C:etc\rc.d\init.d\jenkins\workspace\PipelineJira\target\cucumber\cucumber.json"
 
 :: =========================================
 ::  Create an automation task, run it, send test results to Zephyr.
